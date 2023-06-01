5e84587c70d5178a58e5a7d8d15ce73cc839ae8e
AlfredoVergara10 <112211579+AlfredoVergara10@users.noreply.github.com>
2023-05-31 19:25:13 -0500

    Update mapper.py

--- pregunta_06/mapper.py
+++ pregunta_06/mapper.py
@@ -1,3 +1,22 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+import sys
+
+# Guardar el contenido del archivo en una variable.
+infile = sys.stdin
+
+# Itera sobre cada linea del archivo recibido a través del flujo de entrada.
+for row in infile:
+
+    # Generar una lista de cada una de las columnas de la fila.
+    columns = row.split('   ')
+    
+    # Seleccionar la primera columna como clave.
+    key = columns[0]
+    # Seleccionar la tercer columna como valor.
+    value = columns[2].split('\n')[0]
+
+    # Imprimir la "clave,valor".
+    sys.stdout.write(key + ',' + value + '\n')
