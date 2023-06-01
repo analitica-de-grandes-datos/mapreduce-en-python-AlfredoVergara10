bb236b7f3bf955c1f5460c620f17cca522260f11
AlfredoVergara10 <112211579+AlfredoVergara10@users.noreply.github.com>
2023-05-31 19:38:29 -0500

    Update reducer.py

--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,3 +1,34 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+
+import sys
+
+# Guardar el contenido del archivo en una variable.
+infile = sys.stdin
+
+# Guardar todas las claves y sus coincidencias en un diccionario.
+keys_dict = {}
+
+# Iterar sobre cada clave.
+for row in infile:
+
+    key, value = row.split(',')
+    value = int(value)
+    
+    # Si la clave ya ha sido reconocida, añadir la clave a la lista de claves, sino, actualizar el diccionario.
+    if key in keys_dict:
+        keys_dict[key].append(value)
+    else:
+        keys_dict.update({key: [value]})
+
+# Iterar sobre el diccionario y obtener el resultado.
+for key_ans, value_ans in sorted(keys_dict.items()):
+    
+    # Ordenar las claves de mayor a menor.
+    value_ans.sort()
+    # Obtener una cadena de caracteres a partir de la lista de claves.
+    value_ans = ','.join(map(str, value_ans))
+
+    # Imprimir el resultado.
+    sys.stdout.write(key_ans + '\t' + value_ans + '\n')
