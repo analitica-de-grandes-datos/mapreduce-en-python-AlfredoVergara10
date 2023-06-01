b6da0a92b9ce41a8edf14943cf1e72b701ea3e91
AlfredoVergara10 <112211579+AlfredoVergara10@users.noreply.github.com>
2023-05-31 19:23:23 -0500

    Update mapper.py

--- pregunta_05/mapper.py
+++ pregunta_05/mapper.py
@@ -1,3 +1,20 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+import sys
+
+# Guardar el contenido del archivo en una variable.
+infile = sys.stdin
+
+# Itera sobre cada linea del archivo recibido a través del flujo de entrada.
+for row in infile:
+
+    # Generar una lista de cada una de las columnas de la fila.
+    columns = row.split('   ')
+    
+    # Seleccionar el mes de los valores de la segunda columna como clave.
+    key = columns[1].split('-')[1]
+
+    # Imprimir la "clave,valor".
+    sys.stdout.write(key + ',1' + '\n')
