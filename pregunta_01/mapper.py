32f404591bb04beb1bba87c6b3c2c41ed43e5944
AlfredoVergara10 <112211579+AlfredoVergara10@users.noreply.github.com>
2023-05-31 19:11:08 -0500

    Update mapper.py

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,18 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+import sys
+
+# Guardar el contenido del archivo en una variable.
+infile = sys.stdin
+
+# Itera sobre cada linea del archivo recibido a través del flujo de entrada.
+for row in infile:
+
+    # Generar una lista de cada una de las columnas de la fila.
+    columns = row.split(',')
+    # Seleccionar la columna credit_history.
+    element = columns[2]
+    # Imprimir la "clave,valor".
+    sys.stdout.write(element + ",1\n")
