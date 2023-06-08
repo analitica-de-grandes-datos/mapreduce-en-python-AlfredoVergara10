8e1cbdf0de86410c8f42bf0ef172a9879104d584
AlfredoVergara10 <112211579+AlfredoVergara10@users.noreply.github.com>
2023-06-08 17:45:12 -0500

    Update mapper.py

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -4,6 +4,7 @@
 
 import sys
 
+
 # Guardar el contenido del archivo en una variable.
 infile = sys.stdin
 
@@ -11,13 +12,13 @@ infile = sys.stdin
 for row in infile:
 
     # Generar una lista de cada una de las columnas de la fila.
-    columns = row.split('\n')
+    columns = row.split('\t')
     
     # Seleccionar la primera columna como valor.
     value = columns[0]
 
     # Seleccionar los elementos de la segunda columna como claves.
-    for key in columns[1].split('\n')[0].split(','):
+    for key in columns[1].split('\r')[0].split(','):
         
         # Imprimir la "clave,valor" por cada elemento de la segunda columna.
         sys.stdout.write(key + ',' + value + '\n')
